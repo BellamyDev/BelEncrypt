5b9f6f37b3e1be73d9bcc337b287583714606bdb
BellamyDev <johnbellamydev@gmail.com>
2024-09-19 23:52:57 -0500

    Fixed Menu loop and Verbose Mode

--- BelEncrypt/startMenu.cs
+++ BelEncrypt/startMenu.cs
@@ -4,20 +4,21 @@ public class startMenu
 {
     public static int start()
     {
-        Console.WriteLine("Welcome to BelEncrypt!");
-        Console.WriteLine("Please select an option:");
-        Console.WriteLine("1. Encrypt a file");
-        Console.WriteLine("2. Decrypt a file");
-        Console.WriteLine("3. Toggle verbose mode");
-        Console.WriteLine("4. Exit");
-        Console.Write("Option: ");
-        string option = Console.ReadLine();
+        string option;
         bool repeat = true;
         int value = 0;
         while (repeat == true)
         {
+            Console.WriteLine("Welcome to BelEncrypt!");
+            Console.WriteLine("Please select an option:");
+            Console.WriteLine("1. Encrypt a file");
+            Console.WriteLine("2. Decrypt a file");
+            Console.WriteLine("3. Toggle verbose mode");
+            Console.WriteLine("4. Exit");
+            Console.Write("Option: ");
             //Will repeat until a valid option is selected or the program is exited
             //Will also repeat if verbose mode is toggled
+            option = Console.ReadLine();
             switch (option)
             {
                 case "1":
