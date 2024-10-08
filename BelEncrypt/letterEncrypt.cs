eb2729de9b4daf806d423946ef0cebcf2dde66e0
BellamyDev <johnbellamydev@gmail.com>
2024-10-07 23:16:47 -0500

    Fixed Encryption maps

--- BelEncrypt/letterEncrypt.cs
+++ BelEncrypt/letterEncrypt.cs
@@ -1,92 +1,185 @@
-public class letterEncyrpt{
-    public static string encryptLetter(char letter){
+public class letterEncrypt
+{
+    public static string encryptLetter(char letter)
+    {
         string encryptedLetter;
-       
+
         switch (letter)
         {
             case 'a':
-                encryptedLetter = "z";
-                return encryptedLetter;
+                encryptedLetter = "0x13r";
                 break;
             case 'b':
-                encryptedLetter = "y";
+                encryptedLetter = "0x24y";
                 break;
             case 'c':
-                encryptedLetter = "x";
+                encryptedLetter = "0x35x";
                 break;
             case 'd':
-                encryptedLetter = "w";
+                encryptedLetter = "0x46w";
                 break;
             case 'e':
-                encryptedLetter = "v";
+                encryptedLetter = "0x57v";
                 break;
             case 'f':
-                encryptedLetter = "u";
+                encryptedLetter = "0x68u";
                 break;
             case 'g':
-                encryptedLetter = "t";
+                encryptedLetter = "0x79t";
                 break;
             case 'h':
-                encryptedLetter = "s";
+                encryptedLetter = "0x80s";
                 break;
             case 'i':
-                encryptedLetter = "r";
+                encryptedLetter = "0x91r";
                 break;
             case 'j':
-                encryptedLetter = "q";
+                encryptedLetter = "0x02q";
                 break;
             case 'k':
-                encryptedLetter = "p";
+                encryptedLetter = "0x13p";
                 break;
             case 'l':
-                encryptedLetter = "o";
+                encryptedLetter = "0x24o";
                 break;
             case 'm':
-                encryptedLetter = "n";
+                encryptedLetter = "0x35n";
                 break;
             case 'n':
-                encryptedLetter = "m";
+                encryptedLetter = "0x46m";
                 break;
             case 'o':
-                encryptedLetter = "l";
+                encryptedLetter = "0x57l";
                 break;
             case 'p':
-                encryptedLetter = "k";
+                encryptedLetter = "0x68k";
                 break;
             case 'q':
-                encryptedLetter = "j";
+                encryptedLetter = "0x79j";
                 break;
             case 'r':
-                encryptedLetter = "i";
+                encryptedLetter = "0x80i";
                 break;
             case 's':
-                encryptedLetter = "h";
+                encryptedLetter = "0x91h";
                 break;
             case 't':
-                encryptedLetter = "g";
+                encryptedLetter = "0x02g";
                 break;
             case 'u':
-                encryptedLetter = "f";
+                encryptedLetter = "0x13f";
                 break;
             case 'v':
-                encryptedLetter = "e";
+                encryptedLetter = "0x24e";
                 break;
             case 'w':
-                encryptedLetter = "d";
+                encryptedLetter = "0x35d";
                 break;
             case 'x':
-                encryptedLetter = "c";
+                encryptedLetter = "0x46c";
                 break;
             case 'y':
-                encryptedLetter = "b";
+                encryptedLetter = "0x57b";
                 break;
             case 'z':
-                encryptedLetter = "a";
+                encryptedLetter = "0x68a";
                 break;
             default:
                 encryptedLetter = letter.ToString();
                 break;
         }
-        
+
+        return encryptedLetter;
+    }
+        public static char DecryptLetter(string encryptedLetter)
+    {
+        char decryptedLetter = '\0';
+
+        switch (encryptedLetter)
+        {
+            case "0x13r":
+                decryptedLetter = 'a';
+                break;
+            case "0x24y":
+                decryptedLetter = 'b';
+                break;
+            case "0x35x":
+                decryptedLetter = 'c';
+                break;
+            case "0x46w":
+                decryptedLetter = 'd';
+                break;
+            case "0x57v":
+                decryptedLetter = 'e';
+                break;
+            case "0x68u":
+                decryptedLetter = 'f';
+                break;
+            case "0x79t":
+                decryptedLetter = 'g';
+                break;
+            case "0x80s":
+                decryptedLetter = 'h';
+                break;
+            case "0x91r":
+                decryptedLetter = 'i';
+                break;
+            case "0x02q":
+                decryptedLetter = 'j';
+                break;
+            case "0x13p":
+                decryptedLetter = 'k';
+                break;
+            case "0x24o":
+                decryptedLetter = 'l';
+                break;
+            case "0x35n":
+                decryptedLetter = 'm';
+                break;
+            case "0x46m":
+                decryptedLetter = 'n';
+                break;
+            case "0x57l":
+                decryptedLetter = 'o';
+                break;
+            case "0x68k":
+                decryptedLetter = 'p';
+                break;
+            case "0x79j":
+                decryptedLetter = 'q';
+                break;
+            case "0x80i":
+                decryptedLetter = 'r';
+                break;
+            case "0x91h":
+                decryptedLetter = 's';
+                break;
+            case "0x02g":
+                decryptedLetter = 't';
+                break;
+            case "0x13f":
+                decryptedLetter = 'u';
+                break;
+            case "0x24e":
+                decryptedLetter = 'v';
+                break;
+            case "0x35d":
+                decryptedLetter = 'w';
+                break;
+            case "0x46c":
+                decryptedLetter = 'x';
+                break;
+            case "0x57b":
+                decryptedLetter = 'y';
+                break;
+            case "0x68a":
+                decryptedLetter = 'z';
+                break;
+            default:
+                decryptedLetter = '\0';
+                break;
+        }
+
+        return decryptedLetter;
     }
 }
